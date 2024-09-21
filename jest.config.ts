58edcded8e93c86a288481fc4b8d843e1d014eb6
PowerOfCreation <questerdesura@gmail.com>
2024-09-21 22:43:24 +0200

    jest reporter for github actions

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,25 @@ const config: Config = {
   verbose: true,
   setupFilesAfterEnv: ["<rootDir>/scripts/setupFilesAfterEnv.ts"],
   preset: "jest-expo",
+  coverageReporters: ["json-summary", ["text", { file: "coverage.txt" }]],
+  reporters: [
+    "default",
+    ["github-actions", { silent: false }],
+    "summary",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "coverage",
+        outputName: "jest-junit.xml",
+        ancestorSeparator: " > ",
+        uniqueOutputName: "false",
+        suiteNameTemplate: "{filepath}",
+        classNameTemplate: "{classname}",
+        titleTemplate: "{title}",
+      },
+    ],
+  ],
+  coverageDirectory: "<rootDir>/coverage/",
 }
 
 export default config
