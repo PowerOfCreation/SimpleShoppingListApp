cc214c5daf55ff236a5300803e15bc5f66d47902
PowerOfCreation <questerdesura@gmail.com>
2024-09-23 22:18:45 +0200

    feat: entry editing wip

--- app/index.tsx
+++ app/index.tsx
@@ -2,13 +2,21 @@ import { ActionButton } from "@/components/ActionButton"
 import { Entry } from "@/components/Entry"
 import React from "react"
 import { FlatList, SafeAreaView, StyleSheet } from "react-native"
-import { router } from "expo-router"
+import { router, useNavigation } from "expo-router"
 
 import { ingredientService } from "../api/ingredient-service"
 import { Ingredient } from "@/types/Ingredient"
+import { ThemedText } from "@/components/ThemedText"
+
+function SaveButton() {
+  return <ThemedText>Save</ThemedText>
+}
 
 export default function Index() {
+  const navigation = useNavigation()
+
   const [ingredients, setIngredients] = React.useState<Ingredient[]>([])
+  const [ingredientToEdit, setIngredientToEdit] = React.useState<string>("")
 
   React.useEffect(() => {
     ingredientService.GetIngredients().then((x) => setIngredients(x))
@@ -27,15 +35,32 @@ export default function Index() {
     setIngredients(updatedIngredients)
   }
 
+  const entryLongPress = (id: string) => {
+    setIngredientToEdit(id)
+  }
+
   const renderEntry = ({ item }: { item: Ingredient }) => {
     return (
       <Entry
         ingredientName={item.name}
         isCompleted={item.completed}
         onToggleComplete={() => toggleIngredientCompletion(item.id)}
+        onLongPress={() => entryLongPress(item.id)}
       />
     )
   }
+
+  React.useEffect(() => {
+    if (ingredientToEdit) {
+      navigation.setOptions({
+        headerRight: () => SaveButton(),
+      })
+    } else {
+      navigation.setOptions({
+        headerRight: () => null,
+      })
+    }
+  }, [navigation, ingredientToEdit])
 
   return (
     <SafeAreaView style={styles.container}>
@@ -43,6 +68,7 @@ export default function Index() {
         data={ingredients}
         renderItem={renderEntry}
         keyExtractor={(item) => item.id}
+        extraData={ingredientToEdit}
       />
 
       <ActionButton symbol="+" onPress={() => router.push("/new_ingredient")} />
