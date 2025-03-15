a80ee6e43a9aee36551355cad87ee64a6f73091b
PowerOfCreation <questerdesura@gmail.com>
2025-03-15 01:09:10 +0100

    fix: jest mock expo-font

--- scripts/jestSetupFile.ts
+++ scripts/jestSetupFile.ts
@@ -1,3 +1,4 @@
 import mockAsyncStorage from "@react-native-async-storage/async-storage/jest/async-storage-mock"
 
 jest.mock("@react-native-async-storage/async-storage", () => mockAsyncStorage)
+jest.mock("expo-font") // https://github.com/callstack/react-native-paper/issues/4561#issuecomment-2500877723
