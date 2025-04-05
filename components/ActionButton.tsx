3d6a442728417837ba38708c37c49ac9e45b167d
PowerOfCreation <questerdesura@gmail.com>
2025-04-05 23:12:20 +0200

    refactor: extract styles in RootLayout and ActionButton components

--- components/ActionButton.tsx
+++ components/ActionButton.tsx
@@ -1,4 +1,9 @@
-import { GestureResponderEvent, Text, TouchableOpacity } from "react-native"
+import {
+  GestureResponderEvent,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native"
 import React from "react"
 
 type ActionProps = {
@@ -11,22 +16,29 @@ export function ActionButton(props: ActionProps) {
   return (
     <TouchableOpacity
       testID={props.testID}
-      style={{
-        borderWidth: 1,
-        borderColor: "rgba(0,0,0,0.2)",
-        alignItems: "center",
-        justifyContent: "center",
-        width: 70,
-        position: "absolute",
-        bottom: 10,
-        right: 10,
-        height: 70,
-        backgroundColor: "#fff",
-        borderRadius: 100,
-      }}
+      style={styles.button}
       onPress={props.onPress}
     >
-      <Text style={{ fontSize: 48 }}>{props.symbol}</Text>
+      <Text style={styles.symbol}>{props.symbol}</Text>
     </TouchableOpacity>
   )
 }
+
+const styles = StyleSheet.create({
+  button: {
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0.2)",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 70,
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+    height: 70,
+    backgroundColor: "#fff",
+    borderRadius: 100,
+  },
+  symbol: {
+    fontSize: 48,
+  },
+})
